636ecdea75037ef21ca5040c2e8e9392c9556f42
Eyitayo Afolabi <afobiker.come@yahoo.com>
2019-03-29 08:22:59 +0100

    Commit Deploy.sh for GKE

---
+++ k8s-multi/deploy.sh
@@ -0,0 +1,19 @@
+docker build -t eafolabi/multi-client:latest -t eafolabi/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t eafolabi/multi-server:latest -t eafolabi/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t eafolabi/multi-worker:latest -t eafolabi/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+
+docker push eafolabi/multi-client:latest
+docker push eafolabi/multi-worker:latest
+docker push eafolabi/multi-server:latest
+
+
+docker push eafolabi/multi-client:$SHA
+docker push eafolabi/multi-worker:$SHA
+docker push eafolabi/multi-server:$SHA
+
+kubectl apply -f k8s-multi
+
+kubectl set image deployments/server-deployment server=eafolabi/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=eafolabi/multi-worker:$SHA
+kubectl set image deployments/client-deployment server=eafolabi/multi-client:$SHA
